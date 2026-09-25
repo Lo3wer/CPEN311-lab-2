c00473c09221e916d4d9f95c68ce754c18491745
Ayden <aydennguyen604@gmail.com>
2026-09-24 18:57:03 -0700

    added statemachine code

--- task1/card7seg.sv
+++ task1/card7seg.sv
@@ -1,22 +1,27 @@
 module card7seg(input [3:0] SW, output [6:0] HEX0);
+
+   logic [6:0] hex_out;
+
    always_comb begin
       case (SW)
-         4'b0000: HEX0 = 7'b1111111; // blank
-         4'b0001: HEX0 = 7'b0001000; // Ace -> "A"
-         4'b0010: HEX0 = 7'b0100100; // 2
-         4'b0011: HEX0 = 7'b0110000; // 3
-         4'b0100: HEX0 = 7'b0011001; // 4
-         4'b0101: HEX0 = 7'b0010010; // 5
-         4'b0110: HEX0 = 7'b0000010; // 6
-         4'b0111: HEX0 = 7'b1111000; // 7
-         4'b1000: HEX0 = 7'b0000000; // 8
-         4'b1001: HEX0 = 7'b0010000; // 9
-         4'b1010: HEX0 = 7'b1000000; // 10 -> "0"
-         4'b1011: HEX0 = 7'b1100001; // Jack -> "J"
-         4'b1100: HEX0 = 7'b0011000; // Queen -> "q"
-         4'b1101: HEX0 = 7'b0001001; // King -> "K"
-         default: HEX0 = 7'b1111111; // 1110, 1111: not used -> blank
+         4'b0000: hex_out = 7'b1111111; // blank
+         4'b0001: hex_out = 7'b0001000; // Ace -> "A"
+         4'b0010: hex_out = 7'b0100100; // 2
+         4'b0011: hex_out = 7'b0110000; // 3
+         4'b0100: hex_out = 7'b0011001; // 4
+         4'b0101: hex_out = 7'b0010010; // 5
+         4'b0110: hex_out = 7'b0000010; // 6
+         4'b0111: hex_out = 7'b1111000; // 7
+         4'b1000: hex_out = 7'b0000000; // 8
+         4'b1001: hex_out = 7'b0010000; // 9
+         4'b1010: hex_out = 7'b1000000; // 10 -> "0"
+         4'b1011: hex_out = 7'b1100001; // Jack -> "J"
+         4'b1100: hex_out = 7'b0011000; // Queen -> "q"
+         4'b1101: hex_out = 7'b0001001; // King -> "K"
+         default: hex_out = 7'b1111111; // 1110, 1111: not used -> blank
       endcase
    end
+   assign HEX0 = hex_out;
+
 endmodule
 
--- task5/statemachine.sv
+++ task5/statemachine.sv
@@ -1,3 +1,13 @@
+`define S0 4'b0000
+`define S1 4'b0001
+`define S2 4'b0010
+`define S3 4'b0011
+`define S4 4'b0100
+`define S5 4'b0101
+`define S6 4'b0110
+`define S7 4'b0111
+`define S8 4'b1000
+
 module statemachine(input slow_clock, input resetb,
                     input [3:0] dscore, input [3:0] pscore, input [3:0] pcard3,
                     output load_pcard1, output load_pcard2,output load_pcard3,
@@ -8,6 +18,71 @@ module statemachine(input slow_clock, input resetb,
 // a state machine consists of next state logic, output logic, and the 
 // registers that hold the state.  You will want to review your notes from
 // CPEN 211 or equivalent if you have forgotten how to write a state machine.
+
+    logic [3:0] present_state;
+    logic [7:0] output_vector;
+
+    always_ff @(posedge slow_clock) begin   //update present_state based on game rules
+        if(!resetb)
+            present_state <= `S0;
+
+        else begin
+            case (present_state)
+                `S0: present_state <= `S1;
+                `S1: present_state <= `S2;
+                `S2: present_state <= `S3;
+                `S3: present_state <= `S4;
+                `S4: begin
+                    if (pscore >= 8 || dscore >= 8)
+                        present_state <= `S5;
+                    else if(pscore <= 5)
+                        present_state <= `S6;
+                    else if(dscore <= 5)
+                        present_state <= `S8;
+                    else
+                        present_state <= `S5;
+                end
+                //do nothing at S5 since it's the end, need to press reset to go back to S0
+                `S6: begin
+                    if((dscore == 6 && (pcard3 == 6 || pcard3 == 7))
+                    || (dscore == 5 && (pcard3 >= 4 && pcard3 <= 7))
+                    || (dscore == 4 && (pcard3 >= 2 && pcard3 <= 7))
+                    || (dscore == 3 && pcard3 != 8)
+                    || (dscore >= 0 && dscore <= 2))
+                        present_state <= `S7;
+                    else
+                        present_state <= `S5; //covers both dscore == 7 and all other cases that dont satisfy the bacarat rules to get a 3rd card
+                end
+                `S7: present_state <= `S5;
+                `S8: present_state <= `S5;
+                `S5: present_state <= `S5;
+                default: present_state <= `S0;
+            endcase
+        end
+    end
+    always_comb begin
+        case(present_state)
+            `S1: output_vector = 8'b10000000;
+            `S2: output_vector = 8'b00010000;
+            `S3: output_vector = 8'b01000000;
+            `S4: output_vector = 8'b00001000;
+            `S5: begin 
+                if(pscore > dscore)
+                    output_vector = 8'b00000010;
+                else if (dscore > pscore)
+                    output_vector = 8'b00000001;
+                else
+                    output_vector = 8'b00000011;
+            end
+            `S6: output_vector = 8'b00100000;
+            `S7: output_vector = 8'b00000100;
+            `S8: output_vector = 8'b00000100;
+            default: output_vector = 8'b00000000;
+        endcase
+    end
+
+    //use assign since we need to continuously drive the output wires
+    assign {load_pcard1, load_pcard2, load_pcard3, load_dcard1, load_dcard2, load_dcard3, player_win_light, dealer_win_light} = output_vector;
 
 endmodule
 
